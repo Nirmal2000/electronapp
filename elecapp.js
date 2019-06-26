598539d924d07f51d7b50ffed0cb52d3d251ea0b
Nirmal <nirmalvelu2000@gmail.com>
2019-06-26 14:19:09 +0530

    Resmoved unnecesary comment

--- elecapp.js
+++ elecapp.js
@@ -4,7 +4,7 @@ app.on('ready',()=>{
     win = new BrowserWindow()
     win.loadURL('https://get-url-response.herokuapp.com/')
     win.webContents.session.on('will-download', (event, item, webContents) => {
-        // Set the save path, making Electron not to prompt a save dialog.
+        
         item.setSaveDialogOptions({
             title:"get-url-response"
         })
